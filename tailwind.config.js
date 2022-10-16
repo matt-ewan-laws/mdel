c2d2191477fe59a6934c0ef9644f7537f4eaccf7
Matthew Ewan-Laws <matt@giantpeach.agency>
2022-10-16 21:24:40 +0100

    tailwind + zola

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ['./src/**/*.{css|js}', './templates/**/*.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+  extend: {},
+  },
+  plugins: [require('daisyui')],
+};
