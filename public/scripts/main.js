22c95dc27172eec32cfcf11868e14aff01c5dfc1
Matthew Ewan-Laws <matt@giantpeach.agency>
2023-07-16 17:59:50 +0100

    dark theme

---
+++ public/scripts/main.js
@@ -0,0 +1,29 @@
+function updateLocalStorage() {
+  console.log('test')
+  if (darkModeCheckbox.checked) {
+    localStorage.setItem('theme', 'dark');
+    document.documentElement.setAttribute('data-theme', 'dark');
+  } else {
+    localStorage.setItem('theme', 'cupcake');
+    document.documentElement.setAttribute('data-theme', 'cupcake');
+  }
+}
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  const darkModeCheckbox = document.querySelectorAll('.darkModeCheckbox');
+  darkModeCheckbox.forEach(el => {
+    el.addEventListener('change', updateLocalStorage);
+  })
+
+  // Set theme on page load based on localStorage value
+  const savedTheme = localStorage.getItem('theme');
+  if (savedTheme === 'dark') {
+    darkModeCheckbox.checked = true;
+    document.documentElement.setAttribute('data-theme', 'dark');
+  } else {
+    darkModeCheckbox.checked = false;
+    document.documentElement.setAttribute('data-theme', 'cupcake');
+  }
+
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,6 @@ module.exports = {
   },
   plugins: [require('daisyui')],
   daisyui: {
-    themes: ["retro", "cupcake"]
+    themes: ["retro", "cupcake", "dark"]
   }
 };
