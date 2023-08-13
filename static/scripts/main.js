97166f147743e865197e191ac7720da8f38f81db
Matthew Ewan-Laws <matt.ewanlaws@gmail.com>
2023-08-13 17:56:57 +0100

    fix script

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -1,16 +1,20 @@
+let darkModeCheckbox
+
 function updateLocalStorage() {
-  console.log('test')
-  if (darkModeCheckbox.checked) {
-    localStorage.setItem('theme', 'dark');
-    document.documentElement.setAttribute('data-theme', 'dark');
-  } else {
+  console.log(this.checked)
+  if (this.checked) {
     localStorage.setItem('theme', 'cupcake');
     document.documentElement.setAttribute('data-theme', 'cupcake');
+    console.log(document.documentElement.getAttribute('data-theme'))
+  } else {
+    localStorage.setItem('theme', 'dark');
+    document.documentElement.setAttribute('data-theme', 'dark');
+    console.log(document.documentElement.getAttribute('data-theme'))
   }
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-  const darkModeCheckbox = document.querySelectorAll('.darkModeCheckbox');
+  darkModeCheckbox = document.querySelectorAll('.darkModeCheckbox');
 
   darkModeCheckbox.forEach(el => {
     el.addEventListener('change', updateLocalStorage);
